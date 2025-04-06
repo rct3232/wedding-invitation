d553538d84d1416dd8ad6c00119e5dc4eee4b8fb
rct3232 <rct3232@gmail.com>
2025-04-06 22:38:56 +0900

    complete date counter function

--- src/app/modules/dateCounter.js
+++ src/app/modules/dateCounter.js
@@ -1,57 +1,31 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import styles from "./dateCounter.module.css";
 
 export default function DateCounter (props) {
-  const [counter, setCounter] = useState(0);
   const [timeLeft, setTimeLeft] = useState([0,0,0,0]);
+  const timerId = useRef(null);
   const INTERVAL = 1000;
 
   useEffect(() => {
-    const diff = new Date() - props.date;
-    const oneMinute = 1000 * 60;
+    const computeTime = () => {
+      const diff = props.date - new Date();
+      const oneMinute = 1000 * 60;
+  
+      const d = Math.floor(diff / (oneMinute * 60 * 24));
+      const h = Math.floor((diff % (oneMinute * 60 * 24)) / (oneMinute * 60));
+      const m = Math.floor((diff % (oneMinute * 60)) / oneMinute);
+      const s = Math.floor((diff % oneMinute) / 1000);
+  
+      setTimeLeft([d, h, m, s]);
+    };
 
-    const d = Math.floor(diff / (oneMinute * 60 * 24));
-    const h = Math.floor((diff % (oneMinute * 60 * 24)) / (oneMinute * 60));
-    const m = Math.floor((diff % (oneMinute * 60)) / oneMinute);
-    const s = Math.floor((diff % oneMinute) / 1000);
+    computeTime();
+    timerId.current = setInterval(computeTime, INTERVAL);
 
-    setTimeLeft([d, h, m, s]);
   }, [])
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      const now = [...timeLeft];
-      
-      if(parseInt(now[3]) > 0) now[3] -= 1;
-
-      if (parseInt(now[3]) === 0) {
-        if (parseInt(now[2]) === 0) {
-          if (parseInt(now[1]) === 0) {
-            if (parseInt(now[0]) === 0) clearInterval(timer);
-            else {
-              now[0] -= 1;
-              now[1] = 23;
-              now[2] = 59;
-              now[3] = 59;
-            }
-          } else {
-            now[1] -= 1;
-            now[2] = 59;
-            now[3] = 59;
-          }
-        } else {
-          now[2] -= 1;
-          now[3] = 59;
-        }
-      }
-
-      setTimeLeft(now);
-    }, INTERVAL);
-    return clearInterval(timer);
-  }, [timeLeft])
 
   return (
     <div className="detail">
