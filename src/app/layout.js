9573a539971ea68dc4810088bb8d2676395c7365
Seongbum Seo <seo@seongbum.com>
2025-06-20 09:32:08 +0900

    add opengraph metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,20 @@ import "./globals.css";
 export const metadata = {
   title: "모바일 청첩장",
   description: "모바일 청첩장",
+  openGraph: {
+    title: "모바일 청첩장",
+    description: "박기웅과 권유진의 결혼식에 초대합니다.",
+    url: "https://invitation.plume7eat.xyz/gy28sep2501",
+    type: "website",
+    images: [
+      {
+        url: "https://invitation.plume7eat.xyz/header.png",
+        width: 1200,
+        height: 630,
+        alt: "모바일 청첩장",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
