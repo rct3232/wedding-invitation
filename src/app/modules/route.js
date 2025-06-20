7c0eb2f791ecae53decd60bc483cbea11a0e0662
rct3232 <rct3232@gmail.com>
2025-06-20 10:30:06 +0900

    add map application open feature
    - add navermap, kakaomap, tmap application opener
    - add map application icon

--- src/app/modules/route.js
+++ src/app/modules/route.js
@@ -2,10 +2,13 @@
 
 import React from "react";
 import Map from "./map";
+import styles from './route.module.css';
 
 const clientId = process.env.NEXT_PUBLIC_MAP_CLIENT;
 
 const Route = ({ placeInfo }) => {
+  const encodedName = encodeURIComponent(placeInfo.address.name);
+
   return (
     <div className="detail">
       <div className="header">오시는 길</div>
@@ -14,6 +17,23 @@ const Route = ({ placeInfo }) => {
         <div className="little" style={{ marginLeft: "auto" }}>
           {placeInfo.address.address}
         </div>
+      </div>
+      <div className={styles.mapButtons}>
+        <a
+          href={`nmap://search?query=${encodedName}&appname=invitation.plume7eat.xyz`}
+          aria-label="네이버 지도 열기"
+          className={`${styles.mapButton} ${styles.naver}`}
+        />
+        <a
+          href={`kakaomap://search?q=${encodedName}&p=${placeInfo.map.lat},${placeInfo.map.lng}`}
+          aria-label="카카오맵 열기"
+          className={`${styles.mapButton} ${styles.kakao}`}
+        />
+        <a
+          href={`tmap://search?name=${placeInfo.address.name}`}
+          aria-label="Tmap 열기"
+          className={`${styles.mapButton} ${styles.tmap}`}
+        />
       </div>
       <div className="body">
         {placeInfo.route.map((route, index) => (
