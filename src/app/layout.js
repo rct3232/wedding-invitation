f97a2cbb6ce41b28684976f36db32c2017a6940d
plume7eat <rct3232@gmail.com>
2025-07-11 21:03:34 +0900

    change: remove Header image from git

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
     type: "website",
     images: [
       {
-        url: "https://invitation.plume7eat.xyz/header.jpg",
+        url: "https://invitation.plume7eat.xyz/header/gy28sep2501.jpg",
         width: 1200,
         height: 630,
         alt: "모바일 청첩장",
