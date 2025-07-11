9dfe1ee4af43b363192bb5bae36eb896f63d98c5
plume7eat <rct3232@gmail.com>
2025-07-11 20:32:59 +0900

    change: map application open button
    - add application name text
    - change button layout

--- src/app/modules/route.js
+++ src/app/modules/route.js
@@ -23,17 +23,28 @@ const Route = ({ placeInfo }) => {
           href={`nmap://search?query=${encodedName}&appname=invitation.plume7eat.xyz`}
           aria-label="네이버 지도 열기"
           className={`${styles.mapButton} ${styles.naver}`}
-        />
+        >
+          <span className={styles.iconSmall + ' ' + styles.naverIcon} />
+          <span className="little" style={{ marginLeft: "0.7em"}}>네이버지도</span>
+        </a>
+        <span className={styles.mapButtonDivider}>|</span>
         <a
           href={`kakaomap://search?q=${encodedName}&p=${placeInfo.map.lat},${placeInfo.map.lng}`}
           aria-label="카카오맵 열기"
           className={`${styles.mapButton} ${styles.kakao}`}
-        />
+        >
+          <span className={styles.iconSmall + ' ' + styles.kakaoIcon} />
+          <span className="little" style={{ marginLeft: "0.7em"}}>카카오맵</span>
+        </a>
+        <span className={styles.mapButtonDivider}>|</span>
         <a
           href={`tmap://search?name=${placeInfo.address.name}`}
           aria-label="Tmap 열기"
           className={`${styles.mapButton} ${styles.tmap}`}
-        />
+        >
+          <span className={styles.iconSmall + ' ' + styles.tmapIcon} />
+          <span className="little" style={{ marginLeft: "0.7em"}}>Tmap</span>
+        </a>
       </div>
       <div className="body">
         {placeInfo.route.map((route, index) => (
