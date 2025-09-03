231ebc1de84e257a69b9807f82b81b57fa583572
rct3232 <rct3232@gmail.com>
2025-09-03 11:56:07 +0900

    fix: metadata url fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ export async function generateMetadata({ params }) {
   const path = params.path === undefined ? "fallback" : params.path;
 
   try {
-    const response = await fetch(`http://localhost:${process.env.PORT}/metadata/${path}.json`);
+    const response = await fetch(`https://invitation.plume7eat.xyz/metadata/${path}.json`);
     const data = await response.json();
 
     return {
@@ -26,11 +26,11 @@ export async function generateMetadata({ params }) {
       openGraph: {
         title: "모바일 청첩장",
         description: "김철수와 이영희의의 결혼식에 초대합니다.",
-        url: "https://your-wedding-invitation-url.com",
+        url: "https://invitation.plume7eat.xyz",
         type: "website",
         images: [
           {
-            url: "https://your-wedding-invitation-url.com/fallbackHeader.jpg",
+            url: "https://invitation.plume7eat.xyz/fallbackHeader.jpg",
             width: 1200,
             height: 630,
             alt: "모바일 청첩장",
